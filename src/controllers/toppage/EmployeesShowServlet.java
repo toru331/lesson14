97f37812f1bdf365c02195da35a07077947d9030
toru.hasegawa <pol94941@yahoo.co.jp>
2020-06-24 21:02:50 +0900

    Add Employee show

--- src/controllers/toppage/EmployeesShowServlet.java
+++ src/controllers/toppage/EmployeesShowServlet.java
@@ -1,19 +1,25 @@
 package controllers.toppage;
 
 import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.Employee;
+import utils.DBUtil;
+
 /**
  * Servlet implementation class EmployeesShowServlet
  */
-@WebServlet("/employees/shoe")
+@WebServlet("/employees/show")
 public class EmployeesShowServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -27,7 +33,17 @@ public class EmployeesShowServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	    EntityManager em = DBUtil.createEntityManager();
+
+	    Employee e = em.find(Employee.class, Integer.parseInt(request.getParameter("id")));
+
+	    em.close();
+
+	    request.setAttribute("employee", e);
+
+	    RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/employees/show.jsp");
+	    rd.forward(request, response);
 	}
 
 }
