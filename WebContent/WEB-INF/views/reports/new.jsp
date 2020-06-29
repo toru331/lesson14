38c8706b7d4845fafcb530753c50e3054e29b071
toru331 <66743615+toru331@users.noreply.github.com>
2020-06-29 22:00:15 +0900

    パスワードを表示させないように変更

--- WebContent/WEB-INF/views/reports/new.jsp
+++ WebContent/WEB-INF/views/reports/new.jsp
@@ -7,5 +7,7 @@
          <form method="post" action="<c:url value='/reports/create' />">
              <c:import url="_form.jsp" />
          </form>
+
+         <p><a href="<c:url value='/reports/index' />">一覧に戻る</a></p>
     </c:param>
 </c:import>
--- src/controllers/reports/ReportsCreateServlet.java
+++ src/controllers/reports/ReportsCreateServlet.java
@@ -69,7 +69,7 @@ public class ReportsCreateServlet extends HttpServlet {
 	            request.setAttribute("report", r);
 	            request.setAttribute("errors", errors);
 
-	            RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/views/reports/new.jsp");
+	            RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/reports/new.jsp");
 	            rd.forward(request, response);
 	        } else {
 	            em.getTransaction().begin();
